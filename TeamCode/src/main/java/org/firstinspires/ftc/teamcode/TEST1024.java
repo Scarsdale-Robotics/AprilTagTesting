b088655b5611685b8e6bbc9be95f9a7373ae42da
Your Name <you@example.com>
2025-10-30 14:38:22 -0400

    drive testing and cam testing repo, will be archived at a later date

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST1024.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.drivebase.RobotDrive;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.MotorControlAlgorithm;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@TeleOp(name = "Drive10/29")
+public class TEST1024 extends LinearOpMode {
+    public Motor leftFront;
+    public Motor leftBack;
+    public Motor rightFront;
+    public Motor rightBack;
+    public DriveSubsystem drive;
+    public GoBildaPinpointDriver pinpoint;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = new Motor(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312);
+        rightFront = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312);
+        leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
+        rightBack = new Motor(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
+
+        leftFront.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFront.setRunMode(Motor.RunMode.RawPower);
+        rightFront.setRunMode(Motor.RunMode.RawPower);
+        leftBack.setRunMode(Motor.RunMode.RawPower);
+        rightBack.setRunMode(Motor.RunMode.RawPower);
+
+        leftFront.setInverted(true);
+        rightFront.setInverted(true);
+        leftBack.setInverted(true);
+        rightBack.setInverted(true);
+
+        leftFront.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "Pinpoint");
+        this.drive = new DriveSubsystem(
+                leftFront,
+                rightFront,
+                leftBack,
+                rightBack
+        );
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        pinpoint.setOffsets(6,6, DistanceUnit.INCH);
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        pinpoint.resetPosAndIMU();
+        waitForStart();
+        while (opModeIsActive()) {
+            double forward = gamepad1.left_stick_x;
+            double strafe = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            drive.driveRobotCentricPowers(strafe, forward, turn);
+            telemetry.addData("Strafe", strafe);
+            telemetry.addData("Turn", turn);
+            telemetry.addData("Forward", forward);
+            telemetry.update();
+        }
+    }
+}
